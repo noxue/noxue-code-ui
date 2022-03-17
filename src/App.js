aaae27a6b1ad2cef348ec02ffe9f1992dcedcf53
noxue <173126019@qq.com>
2022-03-17 22:29:59 +0800

    x

--- src/App.js
+++ src/App.js
@@ -3,31 +3,24 @@ import './App.css';
 import React from 'react'
 import axios from 'axios'
 import 'reset-css';
-import AceEditor from "react-ace";
+import CodeMirror from '@uiw/react-codemirror';
+import { StreamLanguage } from '@codemirror/stream-parser';
+import { go } from '@codemirror/legacy-modes/mode/go';
+import { ruby } from '@codemirror/legacy-modes/mode/ruby';
+import { cpp } from "@codemirror/lang-cpp";
+import { python } from "@codemirror/lang-python";
+import { php } from "@codemirror/lang-php";
+import { rust } from '@codemirror/lang-rust';
 
-import "ace-builds/src-noconflict/mode-c_cpp";
-import "ace-builds/src-noconflict/mode-python";
-import "ace-builds/src-noconflict/mode-golang";
-import "ace-builds/src-noconflict/mode-rust";
-import "ace-builds/src-noconflict/mode-ruby";
-import "ace-builds/src-noconflict/mode-perl";
-import "ace-builds/src-noconflict/mode-php";
-import "ace-builds/src-noconflict/mode-sh";
-import "ace-builds/src-noconflict/theme-ambiance";
-import "ace-builds/src-noconflict/theme-chaos";
-import "ace-builds/src-noconflict/theme-github";
-import "ace-builds/src-noconflict/theme-xcode";
-import "ace-builds/src-noconflict/theme-chrome";
-import "ace-builds/src-noconflict/theme-clouds";
 
 import ReactLoading from "react-loading";
 
 class App extends React.Component {
 
   state = {
-    theme: 'ambiance', // 编辑器主题
+    theme: 'dark', // 编辑器主题
     code: '', // 初始代码
-    lang: 'c_cpp|c', // 默认语言
+    lang: 'c', // 默认语言
     stdin: '', // 标准输入内容
     stdout: '', // 标准输出
     stderr: '', // 标准错误输出
@@ -36,26 +29,38 @@ class App extends React.Component {
   }
 
   // 主题列表
-  themes = ["ambiance", "chaos", "github", "xcode", "chrome", "clouds"]
+  themes = ["dark", "light"]
 
-  // 语言列表, name是传给后端的，value是传给ace的
-  langs = [
-    { name: "c", value: "c_cpp" }, { name: "cpp", value: "c_cpp" },
-    { name: "rust", value: "rust" }, { name: "golang", value: "golang" },
-    { name: "python2", value: "python" }, { name: "python3", value: "python" },
-    { name: "ruby", value: "ruby" },
-    { name: "php5", value: "php" }, { name: "php7", value: "php" }, { name: "php8", value: "php" }
-  ]
+  // 语言列表
+  langs = ["c", "cpp", "rust", "golang", "python2", "python3", "php5", "php7", "php8", "ruby"]
+
+  langModes = {
+    "c": cpp(),
+    "cpp": cpp(),
+    "rust": rust(),
+    "golang": StreamLanguage.define(go),
+    "php5": php(),
+    "php7": php(),
+    "php8": php(),
+    "python2": python(),
+    "python3": python(),
+    "ruby": StreamLanguage.define(ruby),
+  }
 
   componentDidMount() {
     const that = this
     // 禁止ctrl+s
     window.addEventListener('keydown', function (e) {
-      if (e.keyCode == 83 && (navigator.platform.match('Mac') ? e.metaKey : e.ctrlKey)) {
+      if (e.keyCode === 83 && (navigator.platform.match('Mac') ? e.metaKey : e.ctrlKey)) {
         e.preventDefault();
         that.runCode(1)
       }
     })
+
+    // 禁止手机浏览器下拉
+    document.addEventListener('touchmove', function (ev) {
+      ev.preventDefault();
+    }, { passive: false });
 
     this.setState({
       code: `#include <stdio.h>
@@ -71,7 +76,7 @@ int main(){
 
     console.log(e.target.value)
     // 切换语言，清空代码和输入输出
-    this.setState({ lang: e.target.value, code:"", stdin:"", stdout:"" });
+    this.setState({ lang: e.target.value, stdin: "", stdout: "" });
   }
   themeChange = (e) => {
     console.log(e.target.value)
@@ -97,9 +102,10 @@ int main(){
 
     axios({
       method: 'post',
-      url: '/api/run',
+      url: process.env.NODE_ENV === "development" ? 'https://code.noxue.com/api/run' : '/api/run',
+
       data: {
-        lang: this.state.lang.split("|")[1],
+        lang: this.state.lang,
         code: this.state.code,
         input: this.state.stdin,
       }
@@ -111,8 +117,6 @@ int main(){
         stdout: res.data.stdout,
         stderr: res.data.stderr,
       })
-
-      console.log(response)
 
       // 请求结束后，切换到输出界面，并清空输入内容
       that.setState({ running: false, tabIndex: 0, stdin: "" });
@@ -126,15 +130,13 @@ int main(){
 
   render() {
 
-    
-
     return (
       <div className="app">
-        <div class="loading" style={{ display: this.state.running ? "block" : "none" }}>
-          <ReactLoading class="ani" type="spinningBubbles" color="#fff" />
+        <div className="loading" style={{ display: this.state.running ? "block" : "none" }}>
+          <ReactLoading className="ani" type="spinningBubbles" color="#fff" />
         </div>
         <div className='tool-bar'>
-          <div class="links">
+          <div className="links">
             <a href='https://noxue.com?code' target="noxue">不学网</a>
             <a href='https://github.com/noxue/noxue-code' target="code">本站源码</a>
           </div>
@@ -148,28 +150,24 @@ int main(){
             </select>
           </div>
           <div className='lang'>
-            <label>编程语言</label>
+            <label>语言</label>
             <select value={this.state.lang} onChange={this.languageChange}>
               {
                 this.langs.map((item, index) => {
-                  const { name, value } = item;
-                  return (<option key={index} value={value + "|" + name}>{name}</option>)
+                  return (<option key={index} value={item}>{item}</option>)
                 })
               }
             </select>
           </div>
         </div>
 
-        <AceEditor
+        <CodeMirror
           value={this.state.code}
-          mode={this.state.lang.split("|")[0]}
-          theme={this.state.theme}
-          onChange={this.codeChange}
-          fontSize={20}
-          name="noxue-code"
-          height='500px'
+          height="500px"
           width='100%'
-          editorProps={{ $blockScrolling: true }}
+          theme={this.state.theme}
+          extensions={[this.langModes[this.state.lang]]}
+          onChange={this.codeChange}
         />
 
         <div className='result'>
